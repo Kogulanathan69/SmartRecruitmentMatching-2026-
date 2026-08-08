c463eec5c2f3bcf937ecee12ee2dc23a4e73608a
Shayanthan <ssayanthan506@gmail.com>
2026-08-08 20:11:50 +0530

    Add company DTOs

---
+++ src/NexHire.Application/DTOs/Company/UpdateCompanyDto.cs
@@ -0,0 +1,17 @@
+namespace NexHire.Application.DTOs.Company;
+
+public class UpdateCompanyDto
+{
+    public string? Name { get; set; }
+    public string? LegalName { get; set; }
+    public string? OfficialEmail { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? RegisteredAddress { get; set; }
+    public string? Description { get; set; }
+    public string? Website { get; set; }
+    public string? Industry { get; set; }
+    public string? CompanySize { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+    public string? LogoUrl { get; set; }
+}
